4f7fbb69e369fb20fabd15c90f20bf92a899dd37
anrosarg <anrosarg@gmail.com>
2025-08-13 21:44:19 -0300

    Panel nuevo integrado

--- backup sistema teocratico/sistema-teocratico/resources/views/inicio.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('welcome')
-
-@section('content')
-    <h1 class="text-3xl font-bold text-gray-800">Bienvenido al sistema</h1>
-    <p class="mt-4 text-gray-600">Usá el menú para ver asignaciones o reuniones.</p>
-@endsection
